489335f8ad9c5f1cb97aeb4f49ecd5ce659c0009
Madni112 <madninenon022@gmail.com>
2026-08-27 17:44:14 +0500

    Define global in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    _global: "globalThis" 
+    global: "globalThis" 
   },
   server: {
     port: 2500
